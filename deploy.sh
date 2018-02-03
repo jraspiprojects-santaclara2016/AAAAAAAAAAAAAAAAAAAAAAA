b6ff7713315305142b55219a873e18806457d3b0
Marc Desch <marc@devem.eu>
2018-02-03 12:44:23 +0100

    adding a deploy script and npm stop command

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo 'Pulling repo off Github...';
+git pull;
+echo 'Installing all dependencies...';
+npm install;
+echo 'Stopping running forever instances...';
+npm stop;
+echo 'Starting a new instance...';
+npm start;
+echo 'deploy.sh script...DONE';
