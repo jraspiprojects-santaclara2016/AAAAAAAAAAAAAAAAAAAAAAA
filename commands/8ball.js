7032cea6eaf16f247f38996a8b4542e8021b29d7
Marc Desch <marc@devem.eu>
2018-02-05 23:01:01 +0100

    add 8ball command

---
+++ commands/8ball.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const config = require('../configuration/config.json');
+
+exports.run = (client, message, args) => {
+    const answers = ['It is certain', 'As I see it, yes', 'Reply hazy try again', 'Don\'t count on it',
+                     'It is decidedly so', 'Most likely', 'Ask again later', 'My reply is no',
+                     'Without a doubt', 'Outlook good', 'Better not tell you now', 'My sources say no',
+                     'Yes definitely', 'Yes', 'Cannot predict now', 'Outlook not so good', 'You may rely on it',
+                     'Signs point to yes', 'Concentrate and ask again', 'Very doubtful'];
+    const index = Math.floor(Math.random()*answers.length);
+    let embed = new Discord.RichEmbed()
+        .setTitle('8ball Command:')
+        .setColor('DARK_GREEN')
+        .addField('8ball says:',answers[index])
+        .setFooter('By ' + config.botName)
+        .setTimestamp()
+    ;
+    message.channel.send({embed}).catch(console.error);
+
+};
