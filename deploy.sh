2ef8a9539c8e8341328cd2f439edd895d85a5c85
Marc Desch <marc@devem.eu>
2018-02-11 13:28:33 +0100

    remove docker for now. maybe add it later.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-echo 'Remove package-lock.json...';
+echo 'rm package-lock.json';
 rm package-lock.json;
 echo 'Pulling repo off Github...';
 git pull;
-echo 'Running docker...';
-docker build Dockerfile
-docker run monikabot
+echo 'Installing all dependencies...';
+npm install;
+echo 'Stopping running forever instances...';
+npm stop;
+echo 'Starting a new instance...';
+npm start;
+echo 'deploy.sh script...DONE';
