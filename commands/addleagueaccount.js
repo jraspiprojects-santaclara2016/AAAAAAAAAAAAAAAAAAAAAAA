4e3013622a97b51c5e2d922c767f7837f8fe0863
Marc Desch <marc@devem.eu>
2018-02-15 21:14:22 +0100

    remove idea folder and changing database layout for league accounts

--- commands/addleagueaccount.js
+++ commands/addleagueaccount.js
@@ -2,8 +2,12 @@ const mariadbHandler = require('../handler/mariadbHandler');
 
 exports.run = (client, message, args , logger) => {
     let region = args[0];
-    let summonerName = args.join(' ').slice(region.length).trim().split(/ +/g);
-    mariadbHandler.functions.addLeagueAccount(summonerName.join(' ').toUpperCase(), region.toUpperCase(), message.author.id).then(data => {
+    let typeAndsummonerName = args.join(' ').slice(region.length).trim().split(/ +/g);
+    let type = typeAndsummonerName[0];
+    let summonerName = typeAndsummonerName.join(' ').slice(type.length).trim().split(/ +/g).join(' ');
+    type = (type =!!'main');
+
+    mariadbHandler.functions.addLeagueAccount(summonerName.toUpperCase(), region.toUpperCase(), message.author.id, type).then(data => {
         logger.verbose(data);
         message.channel.send(`I added ${summonerName.join(' ').toUpperCase()} [${region.toUpperCase()}] to the database`);
     }).catch(error => {
--- commands/listleagueaccounts.js
+++ commands/listleagueaccounts.js
@@ -3,7 +3,7 @@ const mariadbHandler = require('../handler/mariadbHandler');
 exports.run = (client, message, args, logger) => {
     mariadbHandler.functions.getLeagueAccounts(message.author.id).then(data => {
         for(let i = 0; i<data.length;i++) {
-            message.channel.send(`${data[i].summonerName} ${data[i].region}`);
+            message.channel.send(`${data[i].summonerName} ${data[i].region} ${data[i].isMain}`);
         }
     }).catch(error => {
         console.log(error);
