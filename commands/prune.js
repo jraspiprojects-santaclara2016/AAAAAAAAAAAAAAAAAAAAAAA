9dd5f45439fe07b2bb7d41c2f2a564badf91447c
Marc Desch <marc@devem.eu>
2018-02-16 20:21:52 +0100

    improved prune command and somehow fixed the rich presence event

--- commands/prune.js
+++ commands/prune.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 //Require needed config or permission files.
 const config = require('../configuration/config');
 
-//This segment is executed whenever the bot receives a clear command.
 exports.run = (client, message, args) => {
     if (message.member.hasPermission('MANAGE_MESSAGES')) {
         message.channel.messages.fetch({limit: args}).then(data => {
