9eedc7cb74d60867a430db57882e04f3880f9e0f
Marc Desch <marc@devem.eu>
2018-02-17 17:22:01 +0100

    improved addleagueaccount and removeleagueaccount commands.

--- commands/addleagueaccount.js
+++ commands/addleagueaccount.js
@@ -5,11 +5,12 @@ exports.run = (client, message, args , logger) => {
     let typeAndsummonerName = args.join(' ').slice(region.length).trim().split(/ +/g);
     let type = typeAndsummonerName[0];
     let summonerName = typeAndsummonerName.join(' ').slice(type.length).trim().split(/ +/g).join(' ');
+    type = type.toLowerCase();
     type = (type =!!'main');
 
     mariadbHandler.functions.addLeagueAccount(summonerName.toUpperCase(), region.toUpperCase(), message.author.id, type).then(data => {
         logger.verbose(data);
-        message.channel.send(`I added ${summonerName.join(' ').toUpperCase()} [${region.toUpperCase()}] to the database`);
+        message.channel.send(`I added ${summonerName.toUpperCase()} [${region.toUpperCase()}] to the database`);
     }).catch(error => {
         logger.verbose(`${error.code} ${error.sqlMessage}`);
     });
--- commands/removeleagueaccount.js
+++ commands/removeleagueaccount.js
@@ -3,6 +3,7 @@ const mariadbHandler = require('../handler/mariadbHandler');
 exports.run = (client, message, args, logger) => {
     mariadbHandler.functions.deleteLeagueAccount(args.join(' ').toUpperCase(), message.author.id).then(data => {
         console.log(data);
+        message.channel.send(`I removed ${args.join(' ')} from the database`);
     }).catch(error => {
         console.log(error);
     });
