cd11f1e71db8c9206ad56b7eb69969424bdf0192
Marc Desch <me@marcdesch.de>
2018-02-21 15:38:17 +0100

    minor update on presenceUpdate

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -4,7 +4,7 @@ const logger = winstonLogHandler.getLogger();
 exports.run = (client, oldMember, newMember) => {
     if(!newMember) return;
     const memberId = newMember.user.id;
-    if(newMember.presence) return;
+    if(!newMember.presence) return;
     if(newMember.presence.activity.name === 'League of Legends') {
         logger.info('Executing leagueGameInformation');
         const eventFunction = require('../handler/leagueGameInformation');
