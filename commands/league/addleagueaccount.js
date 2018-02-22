0f5a76903c2bb4f1aeffafe78cc3f7d6396f6e8a
Marc Desch <marc@devem.eu>
2018-02-23 00:46:47 +0100

    first working state.

--- commands/league/addleagueaccount.js
+++ commands/league/addleagueaccount.js
@@ -6,42 +6,57 @@ const logger = winstonLogHandler.getLogger();
 module.exports = {
     name: 'addleagueaccount',
     description: 'Link a LeagueOfLegends account to your Discord account',
-    execute(client, message, args) {
+    async execute(client, message) {
         let embed = new Discord.MessageEmbed()
             .setTitle('Add a league account:')
-            .addField('Respond with your summoner name!', 'This message timeouts in 10 seconds.')
+            .addField('Respond with your summoner name!', 'This message timeouts in 20 seconds.')
         ;
-        let filter = m => m.author = message.author;
-        waitingMessage(client, message, embed, filter);
-
-        const region = args[0];
-        const typeAndSummonerName = args.join(' ').slice(region.length).trim().split(/ +/g);
-        let type = typeAndSummonerName[0];
-        const summonerName = typeAndSummonerName.join(' ').slice(type.length).trim().split(/ +/g).join(' ');
-        type = type.toLowerCase();
-        type = (type === 'main');
-
-        mariadbHandler.functions.addLeagueAccount(summonerName.toUpperCase(), region.toUpperCase(), message.author.id, type).then(data => {
-            logger.verbose(data);
-            message.channel.send(`I added ${summonerName.toUpperCase()} [${region.toUpperCase()}] to the database`);
-        }).catch(error => {
-            logger.verbose(`${error.code} ${error.sqlMessage}`);
-        });
+        let response;
+        response = await waitingMessage(client, message, embed);
+        const summonerName = response.first().content;
+        message.channel.send(`Summonername: ${summonerName}`);
+        embed = new Discord.MessageEmbed()
+            .setTitle('Add a league account:')
+            .addField('Respond with your region the account is in!', 'This message timeouts in 20 seconds.')
+        ;
+        response = await waitingMessage(client, message, embed);
+        const region = response.first().content;
+        message.channel.send(`Region: ${region}`);
+        embed = new Discord.MessageEmbed()
+            .setTitle('Add a league account:')
+            .addField('Is this your main? __y__es or __n__o?', 'This message timeouts in 20 seconds.')
+        ;
+        response = await waitingMessage(client, message, embed);
+        const isMain = response.first().content === 'y';
+        message.channel.send(`Main: ${isMain}`);
+        // TODO add API check if account exists!
+        await insertAccountIntoDatabase(message, summonerName, region, isMain, message.author.id);
     },
 };
 
-async function waitingMessage(client, message, embed, filter) {
+async function waitingMessage(client, message, embed) {
     message.channel.send({ embed });
     let response;
     try {
-        response = await message.channel.awaitMessages(filter, {
+        response = await message.channel.awaitMessages(m => m.author === message.author && m.content !== '', {
             max: 1,
-            time: 10000,
+            time: 20000,
             errors: ['time'],
         });
     } catch (err) {
         console.error(err);
         return message.channel.send('No or invalid value entered, cancelling volume selection.');
     }
+    logger.info(response.first().content);
     return response;
+}
+
+async function insertAccountIntoDatabase(message, summonerName, region, type, discordId) {
+    mariadbHandler.functions.addLeagueAccount(summonerName.toUpperCase(), region.toUpperCase(), discordId, type).then(data => {
+        logger.verbose(data);
+        message.channel.send(`I added ${summonerName.toUpperCase()} [${region.toUpperCase()}] to the database`);
+    }).catch(error => {
+        logger.verbose(`${error.code} ${error.sqlMessage}`);
+        message.channel.send(`An error occurred while inserting the values to the database! ${error.sqlMessage}`);
+    });
 }
