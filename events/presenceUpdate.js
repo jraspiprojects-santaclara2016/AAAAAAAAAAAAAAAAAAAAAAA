b96c2b00066a7001091ee85244031c65750a760f
zelle <zeller.fabian.97@web.de>
2018-02-23 22:11:41 +0100

    Fix if the activity of the new Presence of member isn't defined.

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -5,6 +5,7 @@ exports.run = (client, oldMember, newMember) => {
     if(!newMember) return;
     const memberId = newMember.user.id;
     if(!newMember.presence) return;
+    if(!newMember.presence.activity) return;
     if(newMember.presence.activity.name === 'League of Legends') {
         logger.info('Executing leagueGameInformation');
         const eventFunction = require('../handler/leagueGameInformation');
