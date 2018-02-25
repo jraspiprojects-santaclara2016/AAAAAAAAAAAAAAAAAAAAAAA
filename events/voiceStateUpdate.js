b04a233c720c3d16beeb3fa5eacad21371efd0c5
Marc Desch <marcdesch@icloud.com>
2018-02-25 13:47:54 +0100

    adding auto leave when channel is empty

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,7 +3,29 @@ const winstonLogHandler = require('../handler/winstonLogHandler');
 const logger = winstonLogHandler.getLogger();
 const queue = voiceHandler.getQueue();
 
-exports.run = (client, oldMember, newMember) => {
-    logger.debug(queue);
-    logger.debug(newMember);
+exports.run = async (client, oldMember, newMember) => {
+    if(!oldMember.voiceChannel) return;
+    const serverQueue = queue.get(oldMember.guild.id);
+    if(!serverQueue) return;
+    if(serverQueue.voiceChannel === oldMember.voiceChannel) {
+        if(oldMember.id === client.user.id) {
+            await updateVoiceChannel(serverQueue, oldMember, newMember);
+        }
+        if (serverQueue.voiceChannel.members.size === 1) {
+            await leaveVoiceChannel(serverQueue);
+        }
+    }
 };
+
+async function updateVoiceChannel(serverQueue, oldMember, newMember) {
+    if(newMember.voiceChannel !== oldMember.voiceChannel) {
+        logger.debug('I was moved. Updating voice channel information now!');
+        serverQueue.voiceChannel = newMember.voiceChannel;
+    }
+}
+
+async function leaveVoiceChannel(serverQueue) {
+    logger.debug('voiceStateUpdate: Voicechannel now empty... I\'m leaving now.');
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+}
