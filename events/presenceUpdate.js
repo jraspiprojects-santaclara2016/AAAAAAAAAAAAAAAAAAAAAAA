0ca1de7e39aad18e472c5caa6e6216bb783043af
zelle <zeller.fabian.97@web.de>
2018-03-10 14:54:55 +0100

    Disabled the league presence to do anything. Added logging (silly) to observer presence

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -2,7 +2,9 @@ const winstonLogHandler = require('../handler/util/winstonLogHandler');
 const logger = winstonLogHandler.getLogger();
 
 exports.run = (client, oldMember, newMember) => {
-    if (!newMember) return;
+    logger.silly(`Presence update: OldMember: ${oldMember} ## NewMember: ${newMember}`);
+    // TODO currently disabled. Will be enabled at a later version
+/*    if (!newMember) return;
     const memberId = newMember.user.id;
     if (!newMember.presence) return;
     if (!newMember.presence.activity) return;
@@ -10,5 +12,5 @@ exports.run = (client, oldMember, newMember) => {
         logger.info('Presemce: Executing leagueGameInformation');
         const eventFunction = require('../handler/command/leagueGameInformation');
         eventFunction.run(client, memberId);
-    }
+    }*/
 };
