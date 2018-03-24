3cf3031be2a26662c662479c202aeaaa603758e1
zelle <zeller.fabian.97@web.de>
2018-03-24 20:10:59 +0100

    Implemented possible Array in secrets File for API Keys

--- handler/util/secretHandler.js
+++ handler/util/secretHandler.js
@@ -3,25 +3,27 @@ const secrets = require('../../configuration/secrets');
 const winstonLogHandler = require('./winstonLogHandler');
 const logger = winstonLogHandler.getLogger();
 
-
 module.exports = {
     getDatabaseSecrets: function() {
         return secrets.database;
     },
-
     getApiKey: function(keyName) {
-        let key ;
+        let key;
         key = getKeyFromFile(keyName);
-        if(key) return key;
+        if (key) return key;
         key = getKeyFromEnvironment(keyName);
-        if(key) return key;
+        if (key) return key;
         logger.error(`secretHandler: Could not load ApiKey: ${keyName}`);
     },
 };
 
 function getKeyFromFile(keyName) {
     try {
-        return secrets.apiKeys[keyName];
+        const key = secrets.apiKeys[keyName];
+        if (Array.isArray(key)) {
+            return key[Math.floor(Math.random() * key.length)];
+        }
+        return key;
     } catch (error) {
         logger.debug(`secretHandler: Key ${keyName} not found in secrets.json!`);
     }
