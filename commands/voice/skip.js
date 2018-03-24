cba712403f081842fe6e914e9801ac0d43e8f22b
Marc Desch <marc@devem.eu>
2018-03-24 23:21:43 +0100

    add: embeds to voice commands

--- commands/voice/skip.js
+++ commands/voice/skip.js
@@ -1,23 +1,42 @@
+const Discord = require('discord.js');
 const cacheHandler = require('../../handler/util/cacheHandler');
 const winstonLogHandler = require('../../handler/util/winstonLogHandler');
+const messageHandler = require('../../handler/command/discordMessageHandler');
 const logger = winstonLogHandler.getLogger();
 const musicCache = cacheHandler.getMusicCache();
-const discordCustomEmbedHandler = require('../../handler/command/discordCustomEmbedHandler');
 
 module.exports = {
     name: 'skip',
     description: 'Skip to the next song.',
     disabled: false,
-    execute(client, message) {
+    async execute(client, message) {
         const musicQueue = musicCache.get(message.guild.id);
-        if (!musicQueue) {
-            return discordCustomEmbedHandler.run(client, 'Skip', [{
-                name: 'There is nothing playing I could skip for you.',
-                value: '-',
-            }], message.channel);
-        }
-        if (musicQueue.loop) musicQueue.loop = !musicQueue.loop;
-        musicQueue.connection.dispatcher.end();
-        logger.debug('skip: dispatcher end executed.');
+        if (!musicQueue) return await sendNothingPlayingEmbed(message);
+        await skipTrack(musicQueue, message);
     },
 };
+
+async function skipTrack(musicQueue, message) {
+    if (musicQueue.loop) musicQueue.loop = !musicQueue.loop;
+    musicQueue.connection.dispatcher.end();
+    logger.debug('skip: dispatcher end executed.');
+    await sendSkipEmbed(musicQueue, message);
+}
+
+async function sendNothingPlayingEmbed(message) {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('⏭ Skip:')
+        .setColor('DARK_RED')
+        .addField('Error', 'There is nothing playing. Therefore I cannot skip to the next track!')
+    ;
+    messageHandler.sendEmbed('skip', embed, message.channel);
+}
+
+async function sendSkipEmbed(musicQueue, message) {
+    const embed = new Discord.MessageEmbed(musicQueue)
+        .setTitle('⏭ skip:')
+        .setColor('DARK_GREEN')
+        .addField('Success:', 'Skipping song!')
+    ;
+    messageHandler.sendEmbed('skip', embed, message.channel);
+}
--- commands/voice/stop.js
+++ commands/voice/stop.js
@@ -1,24 +1,42 @@
+const Discord = require('discord.js');
 const cacheHandler = require('../../handler/util/cacheHandler');
 const winstonLogHandler = require('../../handler/util/winstonLogHandler');
+const messageHandler = require('../../handler/command/discordMessageHandler');
 const logger = winstonLogHandler.getLogger();
 const musicCache = cacheHandler.getMusicCache();
-const discordCustomEmbedHandler = require('../../handler/command/discordCustomEmbedHandler');
-
 
 module.exports = {
     name: 'stop',
     description: 'Stop the current queue and leave the voice channel.',
     disabled: false,
-    execute(client, message) {
+    async execute(client, message) {
         const serverQueue = musicCache.get(message.guild.id);
-        if (!serverQueue) {
-            return discordCustomEmbedHandler.run(client, 'Stop', [{
-                name: 'There is nothing playing I could stop for you.',
-                value: '-',
-            }], message.channel);
-        }
-        serverQueue.songs = [];
-        serverQueue.connection.dispatcher.end();
-        logger.debug('stop: dispatcher end event called.');
+        if (!serverQueue) return await sendNothingPlayingEmbed();
+        await clearQueueAndStopPlayback(serverQueue);
+        await sendStopEmbed(serverQueue, message);
     },
 };
+
+async function clearQueueAndStopPlayback(serverQueue) {
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+    logger.debug('stop: dispatcher end event called.');
+}
+
+async function sendNothingPlayingEmbed(message) {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('⏹ Stop:')
+        .setColor('DARK_RED')
+        .addField('Error', 'There is nothing playing. Therefore I cannot switch the loop state of the music queue!')
+    ;
+    messageHandler.sendEmbed('stop', embed, message.channel);
+}
+
+async function sendStopEmbed(serverQueue, message) {
+    const embed = new Discord.MessageEmbed(serverQueue)
+        .setTitle('⏹ Stop:')
+        .setColor('DARK_GREEN')
+        .addField('Success:', 'I have stopped the music!')
+    ;
+    messageHandler.sendEmbed('stop', embed, message.channel);
+}
