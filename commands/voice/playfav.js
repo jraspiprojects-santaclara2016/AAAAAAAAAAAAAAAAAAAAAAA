f19ed54739ad53142d99eabe7aa1d3904f037b9f
Marc Desch <marc@devem.eu>
2018-03-28 23:43:38 +0200

    add: playfav from others

--- commands/voice/playfav.js
+++ commands/voice/playfav.js
@@ -1,6 +1,8 @@
+const Discord = require('discord.js');
 const play = require('./play');
 const dbHandler = require('../../handler/util/mariadbHandler');
 const logHandler = require('../../handler/util/winstonLogHandler');
+const messageHandler = require('../../handler/command/discordMessageHandler');
 const logger = logHandler.getLogger();
 
 module.exports = {
@@ -8,16 +10,37 @@ module.exports = {
     description: 'Play your favorite playlist.',
     disabled: false,
     async execute(client, message, args) {
-        if (args.length === 0) return await playFavorite(client, message);
+        if (args.length === 0) return await playFavorite(client, message, message.author.id);
+        if(message.mentions.users.first() === undefined) return await sendWrongFormatEmbed(message);
+        if(message.mentions.everyone) return await sendIllegalMentionEmbed(message);
+        return await playFavorite(client, message, message.mentions.users.first().id);
     },
 };
 
-async function playFavorite(client, message) {
-    dbHandler.functions.getFavPlaylist(message.author.id).then(data => {
+async function playFavorite(client, message, userId) {
+    dbHandler.functions.getFavPlaylist(userId).then(data => {
         if(data[0].favPlaylist) {
             play.execute(client, message, [data[0].favPlaylist]);
         }
     }).catch(error => {
         logger.error(`Error: ${error}`);
     });
+}
+
+async function sendWrongFormatEmbed(message) {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('⭐ Favorite:')
+        .setColor('DARK_RED')
+        .addField('Error', 'Wrong format. Either use 0 Arguments or mention somebody!')
+    ;
+    messageHandler.sendEmbed('playfav', embed, message.channel);
+}
+
+async function sendIllegalMentionEmbed(message) {
+    const embed = new Discord.MessageEmbed()
+        .setTitle('⭐ Favorite:')
+        .setColor('DARK_RED')
+        .addField('Error', 'Do not mention everyone ot here!')
+    ;
+    messageHandler.sendEmbed('playfav', embed, message.channel);
 }
