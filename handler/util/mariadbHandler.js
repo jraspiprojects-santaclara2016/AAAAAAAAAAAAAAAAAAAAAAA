736c39f5ccc695caae79cd1981816e8391b6a9f6
Zelle97 <zeller.fabian.97@web.de>
2018-04-08 17:56:17 +0200

    DBHandler rework stuff

--- handler/util/mariadbHandler.js
+++ handler/util/mariadbHandler.js
@@ -16,117 +16,118 @@ pool.on('release', function(connection) {
 });
 
 const functions = {
-    setFavPlaylist: function(favPlaylist, userId) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const escapedPlaylist = connection.escape(favPlaylist);
-                const setFavPlaylist = `INSERT INTO discordUser (favPlaylist, discordId) VALUES(${escapedPlaylist}, ${userId}) ON DUPLICATE KEY UPDATE favPlaylist = ${escapedPlaylist}`;
-                connection.query(setFavPlaylist, function(error, results) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve(results);
-                    connection.release();
-                });
-            });
-        });
+    setFavPlaylist: async function(favPlaylist, userId) {
+        let connection = await pool.getConnection();
+        const escapedPlaylist = connection.escape(favPlaylist);
+        const setFavPlaylist = `INSERT INTO discordUser (favPlaylist, discordId) VALUES(${escapedPlaylist}, ${userId}) ON DUPLICATE KEY UPDATE favPlaylist = ${escapedPlaylist}`;
+        try {
+            await connection.query(setFavPlaylist);
+            return true;
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing setFavPlaylist: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
-    getFavPlaylist: function(userId) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const getFavPlaylist = `SELECT favPlaylist FROM discordUser WHERE discordId = ${userId}`;
-                connection.query(getFavPlaylist, function(error, results) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve(results);
-                    connection.release();
-                });
-            });
-        });
+    getFavPlaylist: async function(userId) {
+        let connection = await pool.getConnection();
+        const getFavPlaylist = `SELECT favPlaylist FROM discordUser WHERE discordId = ${userId}`;
+        try {
+            //TODO Possible checks
+            return await connection.query(getFavPlaylist);
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing getFavPlaylist: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
-    setGuildPrefix: function(prefix, guildId) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const escapedPrefix = connection.escape(prefix);
-                const setGuildPrefix = `INSERT INTO guildConfiguration (guildId, prefix) VALUES(${guildId}, ${escapedPrefix}) ON DUPLICATE KEY UPDATE prefix = ${escapedPrefix}`;
-                connection.query(setGuildPrefix, function(error, results) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve(results);
-                    connection.release();
-                });
-            });
-        });
+    setGuildPrefix: async function(prefix, guildId) {
+        let connection = await pool.getConnection();
+        const escapedPrefix = connection.escape(prefix);
+        const setGuildPrefix = `INSERT INTO guildConfiguration (guildId, prefix) VALUES(${guildId}, ${escapedPrefix}) ON DUPLICATE KEY UPDATE prefix = ${escapedPrefix}`;
+        try {
+            await connection.query(setGuildPrefix);
+            return true;
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing setGuildPrefix: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
-    getGuildPrefix: function(guildId) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const getGuildPrefix = `SELECT prefix FROM guildConfiguration WHERE guildId = ${guildId}`;
-                connection.query(getGuildPrefix, function(error, results) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve(results);
-                    connection.release();
-                });
-            });
-        });
+    getGuildPrefix: async function(guildId) {
+        let connection = await pool.getConnection();
+        const getGuildPrefix = `SELECT prefix FROM guildConfiguration WHERE guildId = ${guildId}`;
+        try {
+            //TODO possible checks
+            return await connection.query(getGuildPrefix);
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing getGuildPrefix: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
-    getEnableLiveGameStatsForDiscordId: function(discordId) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const selectSummonerNames = `SELECT enableLiveGameStats FROM discordUser WHERE discordUser.discordId = ${connection.escape(discordId)}`;
-                connection.query(selectSummonerNames, function(error, results) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve(results);
-                    connection.release();
-                });
-            });
-        });
+    //TODO depracated?
+    getEnableLiveGameStatsForDiscordId: async function(discordId) {
+        let connection = await pool.getConnection();
+        const selectSummonerNames = `SELECT enableLiveGameStats FROM discordUser WHERE discordUser.discordId = ${connection.escape(discordId)}`;
+        try {
+            //TODO possible checks
+            return await connection.query(selectSummonerNames);
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing getEnableLiveGameStatsForDiscordId: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
-    getLeagueAccountsOfDiscordId: function(discordId) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const selectSummonerNames = `SELECT summonerName, region, isMain FROM summonerNames, discordUser WHERE summonerNames.discordId = discordUser.discordId AND discordUser.discordId = ${connection.escape(discordId)}`;
-                connection.query(selectSummonerNames, function(error, results) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve(results);
-                    connection.release();
-                });
-            });
-        });
+    //TODO depracated?
+    getLeagueAccountsOfDiscordId: async function(discordId) {
+        let connection = await pool.getConnection();
+        const selectSummonerNames = `SELECT summonerName, region, isMain FROM summonerNames, discordUser WHERE summonerNames.discordId = discordUser.discordId AND discordUser.discordId = ${connection.escape(discordId)}`;
+        try {
+            //TODO possible checks
+            return await connection.query(selectSummonerNames);
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing getLeagueAccountsOfDiscordId: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
-    addLeagueAccount: function(summonerName, region, discordId, isMain) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const insertDiscordId = `INSERT INTO discordUser (discordId) VALUES (${connection.escape(discordId)})`;
-                const insertSummonerName = `INSERT INTO summonerNames (summonerName, region, discordId, isMain) VALUES (${connection.escape(summonerName)}, ${connection.escape(region)}, ${connection.escape(discordId)}, ${connection.escape(isMain)})`;
-                connection.query(insertDiscordId);
-                connection.query(insertSummonerName, function(error, results) {
-                    if (error) reject(error);
-                    resolve(results);
-                });
-                connection.release();
-            });
-        });
+    //TODO depracated?
+    addLeagueAccount: async function(summonerName, region, discordId, isMain) {
+        let connection = await pool.getConnection();
+        const insertDiscordId = `INSERT INTO discordUser (discordId) VALUES (${connection.escape(discordId)})`;
+        const insertSummonerName = `INSERT INTO summonerNames (summonerName, region, discordId, isMain) VALUES (${connection.escape(summonerName)}, ${connection.escape(region)}, ${connection.escape(discordId)}, ${connection.escape(isMain)})`;
+        try {
+            await connection.query(insertDiscordId);
+            await connection.query(insertSummonerName);
+            return true;
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing addLeagueAccount: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
-    deleteLeagueAccount: function(summonerName, discordId) {
-        return new Promise(function(resolve, reject) {
-            pool.getConnection(function(err, connection) {
-                const deleteSummonerName = `DELETE FROM summonerNames WHERE summonerName = ${connection.escape(summonerName)} AND discordId = ${connection.escape(discordId)}`;
-                connection.query(deleteSummonerName, function(error, results) {
-                    if (error) reject(error);
-                    resolve(results);
-                });
-                connection.release();
-            });
-        });
+    //TODO depracated?
+    deleteLeagueAccount: async function(summonerName, discordId) {
+        let connection = await pool.getConnection();
+        const deleteSummonerName = `DELETE FROM summonerNames WHERE summonerName = ${connection.escape(summonerName)} AND discordId = ${connection.escape(discordId)}`;
+        try {
+            //TODO possible checks
+            await connection.query(deleteSummonerName);
+            return true;
+        } catch (error) {
+            logger.error('mariadbHandler: Error executing addLeagueAccount: ${error}');
+            return false;
+        } finally {
+            connection.release();
+        }
     },
 };
 
